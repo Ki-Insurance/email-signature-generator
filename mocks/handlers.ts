9f8ce76181ee3ad810c40ef0ba77ea84a6c98e5f
Ollie <ollie.williams@ki-insurance.com>
2022-01-25 21:11:16 +0000

    chore: added a delay to MSW to make loading states more natural

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,7 +1,12 @@
 import { rest } from 'msw'
 
+const RESPONSE = "..."
+
 export const handlers = [
-    rest.post('/api/generate-signature', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ signature: '...'}))
-    })
-  ]
+    rest.post('/api/generate-signature', (req, res, ctx) =>
+        res(
+            ctx.delay(2000),
+            ctx.status(200),
+            ctx.json({ signature: RESPONSE }))
+    )
+]
